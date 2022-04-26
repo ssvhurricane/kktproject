07331a33071724b834c4978d954155c09a4200b6
Rnihurricane <ssvdevelop@yandex.ru>
2022-04-26 21:33:00 +0300

    Add nuklear, stb_image, prepare dir.

---
+++ UI/KKTUI.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+#include <time.h>
+
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
