c2e89761c041cb27c4329c6c263d98c750b4df61
Sergei <ssvdevelop@yandex.ru>
2022-08-19 14:17:03 +0300

    Prepare models.

---
+++ Engine/WorldLayer/ObjectSystem/_Component/MeshComponent/Model/Model.cpp
@@ -0,0 +1,8 @@
+#include "Model.h"
+
+namespace Engine {
+namespace ObjectSystem {
+
+
+} // namespace ObjectSystem
+} // namespace Engine
---
+++ Engine/WorldLayer/ObjectSystem/_Component/MeshComponent/Model/Model.h
@@ -0,0 +1,19 @@
+// OBJ Format.
+#pragma once
+
+#include "../../../../../KKTEngine.h"
+#include "../../../../../CoreLayer/LogSystem/LogSystem.h"
+
+namespace Engine {
+namespace ObjectSystem {
+class Model
+
+{
+private:
+
+public:
+
+};
+
+} // namespace ObjectSystem
+} // namespace Engine
