b96a6a72be70bbd51d8eb25c56455a07b78b15d6
Sergei <ssvdevelop@yandex.ru>
2022-09-28 14:31:39 +0300

    add base input func. files.

---
+++ Engine/InputLayer/TouchInputSystem/TouchInputSystem.h
@@ -0,0 +1,4 @@
+//emscripten_set_touchstart_callback(0,this,1,&touch_cb);
+   // emscripten_set_touchend_callback(0,this,1,&touch_cb);
+   // emscripten_set_touchmove_callback(0,this,1,&touch_cb);
+   // emscripten_set_touchcancel_callback(0,this,1,&touch_cb);
